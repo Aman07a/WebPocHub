308c57a03bae982f2a2ae1e3fc59662420a3b9d8
Aman Ahmed <aman16.aa17@gmail.com>
2023-02-10 11:32:27 +0100

    Implementing Generic Repository Pattern in DAL

---
+++ WebPocHub.Dal/ICommonRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace WebPocHub.Dal
+{
+	public interface ICommonRepository<T>
+	{
+		List<T> GetAll();
+		T GetDetails(int id);
+		void Insert(T item);
+		void Update(T item);
+		void Delete(T item);
+		int SaveChanges();
+	}
+}
