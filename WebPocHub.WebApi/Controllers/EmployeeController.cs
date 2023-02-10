22aa6b2326dab37cecf7a9973e8e93e61cbce3ed
Aman Ahmed <aman16.aa17@gmail.com>
2023-02-10 12:17:09 +0100

    Adding New Controller with D.I. in ASP.NET Core Web API Project

---
+++ WebPocHub.WebApi/Controllers/EmployeeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebPocHub.Dal;
+using WebPocHub.Models;
+
+namespace WebPocHub.WebApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class EmployeeController : ControllerBase
+	{
+		private readonly ICommonRepository<Employee> _commonRepository;
+
+		public EmployeeController(ICommonRepository<Employee> commonRepository)
+		{
+			_commonRepository = commonRepository;
+		}
+
+		[HttpGet]
+		public IEnumerable<Employee> Get()
+		{
+			return _commonRepository.GetAll();
+		}
+
+		[HttpGet("{id:int}")]
+		public Employee GetDetails(int id)
+		{
+			return _commonRepository.GetDetails(id);
+		}
+	}
+}
--- WebPocHub.WebApi/Program.cs
+++ WebPocHub.WebApi/Program.cs
@@ -1,8 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using WebPocHub.Dal;
+using WebPocHub.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+builder.Services.AddDbContext<WebPocHubDbContext>(options => options.UseSqlServer(
+	builder.Configuration.GetConnectionString("MsSqlConStr")));
+builder.Services.AddTransient<ICommonRepository<Employee>, CommonRepository<Employee>>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
