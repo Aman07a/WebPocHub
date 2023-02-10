a4a15af2eb072a8bc0df02d96bf5fac777c2849a
Aman Ahmed <aman16.aa17@gmail.com>
2023-02-10 16:06:03 +0100

    Added TokenManager to Jwt

---
+++ WebPocHub.WebApi/Jwt/TokenManager.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using WebPocHub.Models;
+
+namespace WebPocHub.WebApi.Jwt
+{
+	public class TokenManager : ITokenManager
+	{
+		private readonly SymmetricSecurityKey _key;
+
+		public TokenManager(IConfiguration configuration)
+		{
+			_key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["WebPocHubJWT:Secret"]!));
+		}
+
+		public string GenerateToken(User user, string roleName)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
