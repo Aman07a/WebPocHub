f4dfc3909493063f78d9b9d1f90c4bd9074543e3
Aman Ahmed <aman16.aa17@gmail.com>
2023-02-14 07:27:32 +0100

    Removed comment code

--- WebPocHub.WebApi/Controllers/EmployeesController.cs
+++ WebPocHub.WebApi/Controllers/EmployeesController.cs
@@ -22,30 +22,6 @@ namespace WebPocHub.WebApi.Controllers
 			_mapper = mapper;
 		}
 
-		// [HttpGet]
-		// [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<Employee>))]
-		// [ProducesResponseType(StatusCodes.Status404NotFound)]
-		// public IActionResult Get()
-		// {
-		// 	var employees = _employeeRepository.GetAll();
-
-		// 	if (employees.Count == 0)
-		// 	{
-		// 		return NotFound();
-		// 	}
-
-		// 	return Ok(employees);
-		// }
-
-		// [HttpGet("{id:int}")]
-		// [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Employee))]
-		// [ProducesResponseType(StatusCodes.Status404NotFound)]
-		// public IActionResult GetDetails(int id)
-		// {
-		// 	var employee = _employeeRepository.GetDetails(id);
-		// 	return employee == null ? NotFound() : Ok(employee);
-		// }
-
 		[HttpGet]
 		[ProducesResponseType(StatusCodes.Status200OK)]
 		[ProducesResponseType(StatusCodes.Status404NotFound)]
